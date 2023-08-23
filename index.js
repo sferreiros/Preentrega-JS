6acfa5ca343094a3f6ddeb0c13611589f18e3cc3
sferreiros <sebaferreiros@gmail.com>
2023-08-23 16:23:28 -0300

    js

--- index.js
+++ index.js
@@ -1,114 +1,200 @@
-const  productosElemento = document.querySelector("#productos");
-const carritoElemento = document.querySelector("#carrito");
+const carritoCompras = document.querySelector ("#carritoCompras");
+const compraProductos = document.querySelector("#compraProductos");
+const carritoFoot = document.querySelector("#carritoFoot");
 
-let productos = [
-    {nombre: "Faja", precio: "200", stock: 5},
-    {nombre: "Tobillera", precio: "300", stock: 40},
-    {nombre: "Rodillera", precio: "500", stock: 10},
-    {nombre: "Muslera", precio: "500", stock: 20},
-];
 
+let contador = 1;
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-// Agregamos un contador a cada Producto 
-productos.forEach ((productos) => {
-  productos.contador = 0;});
+let productos = [
+    {id:"1",
+nombre:"Muñequera Turmalina Magnetica Artrosis Artritis",
+precio: "7190",
+imagen:"./img/Muñequera Magnetica.jpeg"},
+{
+    id:"2",
+    nombre:"Rodillera Ortopedica Neoprene Magnetica",
+    precio:"11800",
+    imagen:"./img/Rodillera Ortopedica Neoprene Magnetica.jpeg"
+},
+{
+    id:"3",
+    nombre:"Hombrera Tendinitis Magnetica Hombre Manguito",
+    precio:"11900",
+    imagen:"./img/Hombrera Magnetica.jpeg"
+},
+{
+    id:"4",
+    nombre:"Tobillera Lesión Artrosis Artritis Magnetica",
+    precio:"7990",
+    imagen:"./img/Tobillera Magnetica.jpeg"
+},
+{
+    id:"5",
+    nombre:"Corrector Postural Espalda Clavicula Premiun",
+    precio:"6190",
+    imagen:"./img/Corrector Postural.jpeg"
+},
+{
+    id:"6",
+    nombre:"Faja Termica Lumbar Neoprene Bellenada",
+    precio:"15900",
+    imagen: "./img/Faja Termica Lumbar.jpeg"
+},
+];
 
-// Funcion para sumar en el contador 
-const sumarContador = (index) => {
-     productos[index].contador++;
-     mostrarProductos();
-  }
-
-// Funcion para restar en el contador 
-const restarContador = (index) => {
-  if (productos[index].contador > 0) {
-      productos[index].contador--;
-      mostrarProductos();
-  }
-}
-
-
-// Funcion para aregra productos al carrito
-const agregarProducto = (index) => {
-  productos[index].cantidad = productos[index].contador;
-
-// Verificamos que el contador no sea mayor al Stock 
-if (productos[index].contador > productos[index].stock){
-  return alert(`No hay stock suficiente, el máximo del producto es ${productos[index].stock} disponible`)}
-// Validamos que el usuario agregue mínimo 1 producto
-if (productos[index].contador === 0) {
-  return alert("Debe agregar por lo menos 1 producto al carrito");
-}
-// Agregamos el producto al carrito 
-carrito.push(productos[index]);
-
-
-//Guardamos el carrito en el LocalStorage
-localStorage.setItem("carrito", JSON.stringify(carrito));
-// descontamos la cantidad del carrito al Stock 
-productos[index].stock -= productos[index].contador;
-//Reseteamos el contador 
-productos[index].contador = 0;
-
-
-mostrarProductos();
-mostrarCarrito();
-}
-
-// Mostramos los Productos
+//Mostrar los productos
 const mostrarProductos = () => {
-    productosElemento.innerHTML = " ";
-    productos.forEach((producto, index) => {
-        let productoBox = document.createElement("div");
+    compraProductos.innerHTML = "";
+    productos.forEach ((productos, index) => {
+        let productoBox = document.createElement ("div");
         productoBox.innerHTML = `
-          <p>Nombre:${producto.nombre}</p>
-          <p>Precio: ${producto.precio}</p> 
-          <p>Stock: ${producto.stock}</p>
-          <p>Cantidad: ${producto.contador}</p> 
-          `;
-        productosElemento.appendChild(productoBox);
-
-        let btnSumar = document.createElement("button");
-        btnSumar.innerHTML = "+";
-        btnSumar.classList.add ("btn","btn-sm","btn-secondary");
-        productoBox.appendChild(btnSumar);
-        btnSumar.onclick = () => sumarContador(index);
-
-        let btnResta = document.createElement("button");
-        btnResta.innerHTML = "-";
-        btnResta.classList.add ("btn","btn-sm","btn-secondary");
-        productoBox.appendChild(btnResta);
-        btnResta.onclick = () => restarContador(index);
+        <div class="col">
+        <div class="card">
+          <div class="card-body" >
+            <div>
+              <img src="${productos.imagen}" class="card-img-top" alt="Muñequera">
+            <h5 class="card-title">${productos.nombre}</h5>
+            <p class="card-text">${productos.precio}</p>
+            <button class="btn btn-dark btn-comprar" data-index="${index}"">Comprar</button>
+            </div>
+          </div>
+        </div>
+      </div>`;
+      compraProductos.appendChild(productoBox);
       
-        let btnAgregar = document.createElement("button");
-        btnAgregar.innerHTML = "Agregar";
-        btnAgregar.classList.add ("ms-3", "btn", "btn-primary","d-grid" ,"gap-2")
-        productoBox.appendChild(btnAgregar);
-        
-        btnAgregar.onclick = () => agregarProducto(index);
-      });
-}
-
-// Mostramos el carrito
-const mostrarCarrito = () => {
-  carritoElemento.innerHTML = " ";
-
-  carrito = JSON.parse(localStorage.getItem("carrito")) || [];
-  
-  carrito.forEach((producto) => {
-    let productoBox = document.createElement("div");
-    productoBox.innerHTML = `
-    <p>Nombre:${producto.nombre}</p>
-    <p>Precio: ${producto.precio}</p> 
-    <p>Cantidad: ${producto.cantidad}</p> 
-    <p>Subtotal: ${producto.precio * producto.cantidad}</p> 
-    <p>-------------------------------------------</p> 
-    `;
-    carritoElemento.appendChild(productoBox);
-  });
+      
+   });
+   // Agregamos evento para el btn de compra 
+   const btnsComprar = document.querySelectorAll('.btn-comprar');
+   btnsComprar.forEach(btn => {
+       btn.addEventListener('click', agregarProducto);
+   });
+   
+   
 };
 
-mostrarCarrito();
-mostrarProductos();
+// Agregamos producto al carrito
+const agregarProducto = (e) => {
+    const index = event.target.getAttribute('data-index');
+    productos[index].cantidad = 1;
 
+    carrito.push(productos[index]);
+    localStorage.setItem("carrito",JSON.stringify(carrito));
+    mostrarCarrito();
+    totalCarrito ();
+    
+};
+
+//Mostrar el carrito
+const mostrarCarrito = () => {
+    carritoCompras.innerHTML = "";
+  
+    carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+  
+    carrito.forEach((productos, index) => {
+        let productoBox = document.createElement ("tr")
+        productoBox.innerHTML = `        
+        
+                <th scope="row">${productos.id}</th>
+                <td>${productos.nombre}</td>
+                <td><span class="contador">${productos.cantidad}</span></td>
+                <td>
+                    <button class="btn btn-success btnSuma" data-index="${index}">+</button>                    
+                    <button class="btn btn-danger btnResta" data-index="${index}">-</button>
+                </td>
+                <td>$ <span>${productos.precio* productos.cantidad}  </span></td>
+        `;
+  
+    carritoCompras.appendChild(productoBox);  
+        
+    });
+  // Agregamos evento para el btn de suma y resta
+  const btnsSuma = document.querySelectorAll('.btnSuma');
+  const btnsResta = document.querySelectorAll('.btnResta');
+
+  btnsSuma.forEach(btn => {
+       btn.addEventListener('click', sumarCantidad);
+   });
+   btnsResta.forEach(btn => {
+    btn.addEventListener(`click`, restarCantidad);
+   });  
+  };
+
+// Función para sumar 
+const sumarCantidad = (e) => {
+    const index = e.target.getAttribute('data-index');
+    carrito[index].cantidad++;
+    actualizarCarrito();
+    totalCarrito ();
+};
+
+// Función para restar 
+const restarCantidad = (e) => {
+    const index = e.target.getAttribute('data-index');
+    if (carrito[index].cantidad > 1) {
+        carrito[index].cantidad--;
+        
+    } else {
+        carrito.splice(index, 1);
+    }
+    actualizarCarrito();
+    totalCarrito ();
+};
+// Función para actualizar el carrito 
+const actualizarCarrito = () => {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    mostrarCarrito();
+};
+// Función para total del carrito
+const calcularTotalCarrito = () => {
+    let total = 0;
+    carrito.forEach(producto => {
+        total += producto.precio * producto.cantidad;
+    });
+    return total;
+};
+
+// Mostramos el total del Carrito
+const totalCarrito = () => {
+    carritoFoot.innerHTML = " ";
+
+    let totalCantidad = 0; // Variable para almacenar la suma de cantidades
+    carrito.forEach(producto => {
+        totalCantidad += producto.cantidad;
+    });
+    
+    let totalPrecio = calcularTotalCarrito(); // Obtener el total de precio
+
+    let productoBox = document.createElement ("tr")
+    productoBox.innerHTML =`
+    <th scope="row" colspan="2">Total productos</th>
+        <td>${totalCantidad}</td>
+        <td>
+            <button class="btn btn-outline-danger" id="vaciar-carrito">
+                Vaciar Carrito
+            </button>
+            <button class="btn btn-outline-primary" id="comprar">Comprar</button>
+        </td>
+        <td class="font-weight-bold">$ <span>${totalPrecio}</span></td>
+        `;
+        carritoFoot.appendChild(productoBox); 
+        const btnVaciar = document.getElementById(`vaciar-carrito`);
+btnVaciar.addEventListener (`click`, () => {
+    localStorage.removeItem("carrito");
+    mostrarCarrito();
+    totalCarrito ();   
+
+});  
+
+const btnComprar = document.getElementById ("comprar");
+btnComprar.addEventListener (`click`, () => {
+    window.location.href = "./page/carrito_compras.html"
+});          
+    
+};
+
+
+mostrarCarrito();
+mostrarProductos ();
+totalCarrito ();    
